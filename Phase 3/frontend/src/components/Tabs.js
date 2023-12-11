c15eea7c7fdcfd7c9c39047473f794259c2c286e
wmccrthy <wyattmccarthy2020@gmail.com>
2023-12-11 09:25:24 -0500

    mobile UI fix

--- Phase 3/frontend/src/components/Tabs.js
+++ Phase 3/frontend/src/components/Tabs.js
@@ -61,7 +61,7 @@ const Tabs = () => {
                 }}>Data Entry</button>
                 
             </div> */}
-            <div id="nav" className="fixed z-10 top-0 md:left-0 flex md:flex-col align-center justify-evenly w-full md:w-20 md:h-full  bg-gray-800 border-r  border-gray-300 text-sm font-bold dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 text-white">
+            <div id="nav" className="fixed z-10 top-0 md:left-0 flex md:flex-col align-center items-center justify-evenly w-full md:w-20 h-28  md:h-full  bg-gray-800 border-b md:border-r  border-gray-300 text-sm font-bold dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 text-white">
                 {/* <div id="indicator" className="w-10 h-1/6 opacity-0 absolute top-0 left-0  bg-white indic"></div> */}
                 <button className="h-fit w-full flex items-center justify-center" onClick={(e) => {
                     handleClick(<QueryPlayerAll/>, e)
